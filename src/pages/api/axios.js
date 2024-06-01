0cc5d939c12320fd45883c9fb96b01e6d8f1bb79
DatNguyen1231 <129192090+DatNguyen1231@users.noreply.github.com>
2024-06-02 00:58:14 +0700

    update

--- src/pages/api/axios.js
+++ src/pages/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://cgdn-springboot-jwt-mysql-master-production.up.railway.app',
 });
 
 axiosInstance.interceptors.request.use(
